7479283fd90c5e54100f2e2f24281b0d6cde7cf9
0xWerz <mouomrane@gmail.com>
2025-12-06 21:06:11 +0100

    Add remotePatterns for SSRF testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Allow httpbin.org for SSRF testing
+    // httpbin.org/redirect-to?url=X follows redirects to arbitrary URLs
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'httpbin.org',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
